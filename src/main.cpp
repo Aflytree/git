e9e740bf5f77ba1e56b58be955864297970c1bb1
lynxi <aifei.zhang@lynxi.com>
2019-07-10 09:44:26 +0800

    removeNthFromEnd is ok

--- src/main.cpp
+++ src/main.cpp
@@ -54,7 +54,7 @@ int main(int argc, char *argv[])
 //        printf("error in isSameTreeTest\n");
 //    }
 //
-    binarySearchTest();
+//    binarySearchTest();
     removeNthFromEndTest();
     return 0;
 }
--- src/removeNthFromEnd.cpp
+++ src/removeNthFromEnd.cpp
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "global.h"
 
 #define TEST(x, y) printf("%12d --->> %-12d  %s\r\n", x, A.reverse(x), y==A.reverse(x)? "Successed":"Failed")
-
-
+// 1 -> 2 -> 3 -> 4 ->5
+// 1 -> 2 -> 4 -> 5 删除倒数第二个
+//
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n)
@@ -19,7 +21,7 @@ public:
         
         for(int i = 0;i < n;i++)
         {
-            if(p == NULL)return NULL;
+            if(q == NULL)return NULL;
             q = q->next; 
         }
 
@@ -38,7 +40,48 @@ public:
 void removeNthFromEndTest()
 {
     Solution A;
-   // if(1 == A.isSameTree(p, q))
+    ListNode *listNode, *tmp, *prev=NULL;
+    ListNode *p;
+
+    for(int i = 0;i < 10;i++)
+    {
+        p = (ListNode *) malloc(sizeof(*p));  
+        p->val = i + 10;
+        p->next = NULL;
+       // ListNode willInsert(i+10);
+       // p = &willInsert;
+
+        if(i == 0)
+            listNode = p;
+        if(prev != NULL)
+            prev->next = p;
+        prev = p;
+    }
+    //free(p);
+    // for(int j = 0; j < 10;j++)
+    // {
+    //     free(p);
+   //     p = p->next;
+   // }
+   
+    printf("listNode->val =%d\n", listNode->val);
+    tmp = listNode; 
+
+    while(tmp != NULL)
+    {
+        printf("tmp->val =%d\n", tmp->val);
+        tmp = tmp->next;
+    }
+
+    listNode = A.removeNthFromEnd(listNode, 2);
+    
+    while(listNode != NULL)
+    {
+        printf("after listNode->val =%d\n",listNode->val);
+        listNode = listNode->next;
+    }
+
+    // if(1 == A.isSameTree(p, q))
      //   printf("is same tree\n");
    // else
      //   printf("is not same tree\n");
